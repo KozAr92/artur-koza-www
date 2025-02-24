17487269dc781ce7c91b3d062743449be1b364cd
Artur Koza <artur.koza.developer@gmail.com>
2025-02-24 22:14:14 +0100

    deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import process from 'process';
 
 const repoName = 'artur-koza-www';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: (process.env.NODE_ENV as string) === 'production' ? `/${repoName}/` : '/',
+  base: `/${repoName}/`,
   plugins: [react(), svgr()],
   assetsInclude: ["**/*.JPG", "**/*.jpg"],
   optimizeDeps: {
